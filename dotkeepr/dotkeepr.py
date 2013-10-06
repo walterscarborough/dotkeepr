c409cabbd5e631f31c5979df81f008ac3e9f7adf
Walter Scarborough <walter.scarborough@gmail.com>
2013-10-06 17:24:54 -0500

    - Fixed config path issue when running dotkeepr outside of project   directory

--- dotkeepr/dotkeepr.py
+++ dotkeepr/dotkeepr.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 
+# System Imports
+import os
+
 # Project Imports
 from parser import dotfile_config_parser
 
-config_file = '../config/config.ini'
+base_path = os.path.dirname(__file__)
+config_file = os.path.abspath(os.path.join(base_path, '../config/config.ini'))
 
 dotfiles = dotfile_config_parser.read_config_file_into_list(config_file)
 
